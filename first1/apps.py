7f3eecfebc4ac79323d8b1aa8a6b71643c257bdd
Your Name <youremail@domain.com>
2022-05-31 19:49:37 +0600

    added first1 app

--- first1/apps.py
+++ first1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Main1Config(AppConfig):
+class First1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main1'
+    name = 'first1'
